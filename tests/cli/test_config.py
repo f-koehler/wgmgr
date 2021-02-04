1dc8e2c8698d318594d32fe75eed9da1b16eb3fc
Fabian Köhler <fabian.koehler@protonmail.ch>
2021-02-04 15:59:05 +0100

    Add some unit tests for config subcommand.

--- tests/cli/test_config.py
+++ tests/cli/test_config.py
@@ -1,5 +1,7 @@
 import tempfile
+from pathlib import Path
 
+import pytest
 from typer.testing import CliRunner
 
 from wgmgr.cli.config import app
@@ -7,24 +9,56 @@ from wgmgr.cli.config import app
 runner = CliRunner()
 
 
-def test_ansible_group():
-    with tempfile.TemporaryFile() as tmp:
+def test_config_new():
+    # new config without any subnet should fail
+    with tempfile.TemporaryDirectory() as tmp:
+        path = Path(tmp) / "config.yml"
         result = runner.invoke(
             app,
-            ["new", "--backend", "ansible_group", "--backend-option", f"path:{tmp}"],
+            ["new", "-c", path],
+        )
+        assert result.exit_code != 0
+
+    # invalid IPv4 subnet should fail
+    with tempfile.TemporaryDirectory() as tmp:
+        path = Path(tmp) / "config.yml"
+        result = runner.invoke(app, ["new", "-c", path, "-4", "WW.XX.YY.ZZ"])
+        assert result.exit_code != 0
+
+    # valid IPv4 subnet should succeed
+    with tempfile.TemporaryDirectory() as tmp:
+        path = Path(tmp) / "config.yml"
+        result = runner.invoke(app, ["new", "-c", path, "-4", "10.0.0.0/24"])
+        assert result.exit_code == 0
+
+
+def test_config_set_default_port():
+    # modifying non-existent config file should fail
+    with tempfile.TemporaryDirectory() as tmp:
+        path = Path(tmp) / "config.yml"
+        result = runner.invoke(
+            app,
+            ["set", "default-port", "-c", path, "1024"],
+        )
+        assert result.exit_code != 0
+
+    # setting invalid ports should fail
+    with tempfile.TemporaryDirectory() as tmp:
+        path = Path(tmp) / "config.yml"
+        result = runner.invoke(
+            app,
+            ["new", "-c", path, "-4", "10.0.0.0/24"],
         )
         assert result.exit_code == 0
 
         result = runner.invoke(
             app,
-            [
-                "set",
-                "default-port",
-                "--backend",
-                "ansible_group",
-                "--backend-option",
-                f"path:{tmp}",
-                4242,
-            ],
+            ["set", "default-port", "-c", path, "-1"],
         )
-        assert result.exit_code == 0
+        assert result.exit_code != 0
+
+        result = runner.invoke(
+            app,
+            ["set", "default-port", "-c", path, "90000"],
+        )
+        assert result.exit_code != 0
